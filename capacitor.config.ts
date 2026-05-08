c71876b35c984e2582efdfee9c5e39adb6e7aafd
NicolasCh25 <nicolas.chiguano@epn.edu.ec>
2026-05-08 14:12:11 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'movieapp',
+  webDir: 'www'
+};
+
+export default config;
